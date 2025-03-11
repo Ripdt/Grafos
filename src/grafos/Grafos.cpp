df0dee898c33230cc3df5b239b694ff4d822d259
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2025-03-11 20:22:52 +0000

    base menu imp

--- src/grafos/Grafos.cpp
+++ src/grafos/Grafos.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int escolha, op;
+
+    do {
+        cout << "\n===== MENU PRINCIPAL =====\n";
+        cout << "1 - Usar Lista de Adjacência\n";
+        cout << "2 - Usar Matriz de Adjacência\n";
+        cout << "3 - Sair\n";
+        cout << "Escolha uma opção: ";
+        cin >> escolha;
+
+        switch (escolha) {
+            case 1:
+                do {
+                    cout << "\n--- Lista de Adjacência ---\n";
+                    cout << "1 - Adicionar aresta\n";
+                    cout << "2 - Exibir grafo\n";
+                    cout << "3 - Voltar ao menu principal\n";
+                    cout << "Escolha: ";
+                    cin >> op;
+
+                    switch (op) {
+                        case 1:
+                            cout << "Opção de adicionar aresta escolhida.\n";
+                            break;
+                        case 2:
+                            cout << "Opção de exibir grafo escolhida.\n";
+                            break;
+                        case 3:
+                            cout << "Voltando ao menu principal...\n";
+                            break;
+                        default:
+                            cout << "Opção inválida! Tente novamente.\n";
+                    }
+                } while (op != 3);
+                break;
+
+            case 2:
+                do {
+                    cout << "\n--- Matriz de Adjacência ---\n";
+                    cout << "1 - Adicionar aresta\n";
+                    cout << "2 - Exibir grafo\n";
+                    cout << "3 - Voltar ao menu principal\n";
+                    cout << "Escolha: ";
+                    cin >> op;
+
+                    switch (op) {
+                        case 1:
+                            cout << "Opção de adicionar aresta escolhida.\n";
+                            break;
+                        case 2:
+                            cout << "Opção de exibir grafo escolhida.\n";
+                            break;
+                        case 3:
+                            cout << "Voltando ao menu principal...\n";
+                            break;
+                        default:
+                            cout << "Opção inválida! Tente novamente.\n";
+                    }
+                } while (op != 3);
+                break;
+
+            case 3:
+                cout << "Saindo do programa...\n";
+                break;
+
+            default:
+                cout << "Opção inválida! Tente novamente.\n";
+        }
+    } while (escolha != 3);
+
+    return 0;
 }
